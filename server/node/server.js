ab68088ea27f879dd09a00b36f9f37b9d389232c
necfol <necfoll@gmail.com>
2017-11-09 11:23:33 +0800

    format

--- server/node/server.js
+++ server/node/server.js
@@ -3,23 +3,23 @@ const url = require('url')
 const querystring = require('querystring')
 var delay = (timeout) => {
     return new Promise(resolve => {
-        setTimeout(function() {
+        setTimeout(function () {
             resolve(true)
         }, timeout);
     })
 }
-var app = async  (req, res) => {
+var app = async(req, res) => {
     var path = url.parse(req.url).pathname
-    if(path == '/') {
-        res.write("hello world") 
+    if (path == '/') {
+        res.write("hello world")
     }
-    if(path == '/query') {
+    if (path == '/query') {
         var queryParams = querystring.parse(url.parse(req.url).query)
-        if(queryParams.sleep == 'true') {
+        if (queryParams.sleep == 'true') {
             await delay(5000)
             res.write("hello world")
         } else {
-            res.write("normal")            
+            res.write("normal")
         }
     }
     res.end()
