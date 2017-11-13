fbf83a006a608ffad914b590781855b21c6c03a3
necfol <necfoll@gmail.com>
2017-11-13 14:47:45 +0800

    add fib tcp server

---
+++ net/fib/tcp.js
@@ -0,0 +1,11 @@
+const net = require('net')
+var func = (conn) => {
+    var data
+    while (data = conn.read()) {
+        console.log(data)
+        conn.write('fuck\n')    
+    }
+}
+var nsvr = new net.TcpServer('7777', func)
+nsvr.run((err, result) => {})
+console.log('ok')
